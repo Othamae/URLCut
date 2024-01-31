1e31e4d238a861dd3c68f7063ae9b017e542d440
Othamae <vero.gconesa@gmail.com>
2024-01-31 16:10:37 +0100

    feat: :sparkles: Created linkShortener component

---
+++ components/LinkShortener.tsx
@@ -0,0 +1,12 @@
+import CardComponent from './CardComponent'
+import FormLink from './FormLink'
+
+const LinkShortener = () => {
+	return (
+		<CardComponent title="Shorten a long link" description="Create a short link to share anywhere.">
+			<FormLink />
+		</CardComponent>
+	)
+}
+
+export default LinkShortener
