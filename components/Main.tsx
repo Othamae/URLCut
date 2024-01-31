1314642302584adcd0a9e9fa3c8f193bb4d65c25
Othamae <vero.gconesa@gmail.com>
2024-01-31 21:41:19 +0100

    feat: :sparkles: Changed description in main

--- components/Main.tsx
+++ components/Main.tsx
@@ -3,13 +3,13 @@ import TabsComponent from "./TabsComponent"
 
 const Main = async () => {
     return (
-        <main className="max-w-5xl flex items-center justify-center mx-auto flex-col pt-8 ">
+        <main className="max-w-5xl flex items-center justify-center mx-auto flex-col pt-4">
             <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-2 font-bold">
                 Make sharing links
                 <span className="text-pink-500"> easy</span>
             </h1>
-            <p className="mb-4 text-neutral-800/70">
-                Create short links, QR Codes, and Link-in-bio pages. Share them anywhere.
+            <p className="mb-4 text-neutral-800/70 text-center">
+                Create short links and QR Codes. Share them anywhere!
             </p>
             <TabsComponent />
         </main>
