6828c0415b1a9bf56781b59976d3cc889556c793
Othamae <vero.gconesa@gmail.com>
2024-01-31 21:41:45 +0100

    feat: :sparkles: Added validation if formData is empty

--- components/FormLink.tsx
+++ components/FormLink.tsx
@@ -4,16 +4,23 @@ import { LinkIcon } from 'lucide-react'
 import Link from 'next/link'
 import { useState } from 'react'
 import FormButton from './FormButton'
+import InputError from './InputError'
 import { CardContent, CardFooter } from './ui/card'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
 
 const FormLink = () => {
 	const [shortURL, setShortURL] = useState('')
+	const [error, setError] = useState('')
 	const onSubmit = async (formData: FormData) => {
 		const longURL = formData.get('longURL') as string
+		if (!longURL) {
+			setError('Please insert a long URL')
+			return
+		}
 		const link = await createShortURL(longURL)
 		setShortURL(link)
+		setError('')
 	}
 	return (
 		<form action={onSubmit}>
@@ -24,18 +31,20 @@ const FormLink = () => {
 							Long URL
 						</Label>
 						<Input name="longURL" id="longURL" placeholder="Insert a long URL" />
+						{error && <InputError id='longURL' error={error} />}
 					</div>
 				</div>
 			</CardContent>
-			<CardFooter className="flex justify-start gap-4 mt-4">
+			<CardFooter className="flex justify-start gap-4 mt-4 md:flex-row flex-col">
 				<FormButton text="Short it!" />
+
 				{shortURL && (
 					<Link
 						href={shortURL}
 						target="_blank"
-						className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-xl ring-offset-background items-center hover:text-blue-600"
+						className="flex h-10 w-full px-1 rounded-md border border-input bg-background md:px-3 py-2 md:text-xl ring-offset-background items-center hover:text-blue-600"
 					>
-						<LinkIcon className="h-4 w-4 mr-2 " />
+						<LinkIcon className="h-4 w-4 mr-2 md:block hidden " />
 						{shortURL}
 					</Link>
 				)}
--- components/QRCodeForm.tsx
+++ components/QRCodeForm.tsx
@@ -1,15 +1,21 @@
 'use client'
 import { getQRCode } from '@/actions/actions'
+import { ArrowBigRight } from 'lucide-react'
 import { useState } from 'react'
 import FormButton from './FormButton'
+import InputError from './InputError'
 import { CardContent, CardFooter } from './ui/card'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
 const QRCodeForm = () => {
     const [qrCode, setQrCode] = useState('')
+    const [error, setError] = useState('')
     const onSubmit = async (formData: FormData) => {
         const longUrl = formData.get('longURLtoQR') as string
-
+        if (!longUrl) {
+            setError('Please insert a long URL')
+            return
+        }
         const base64String = await getQRCode(longUrl)
 
         const dataUrl = `data:image/png;base64,${base64String}`
@@ -24,19 +30,29 @@ const QRCodeForm = () => {
                             Enter your QR Code destination
                         </Label>
                         <Input name="longURLtoQR" id="longURLtoQR" placeholder="Insert a long URL" />
+                        {error && <InputError id='longURL' error={error} />}
                     </div>
                 </div>
             </CardContent>
-            <CardFooter className="flex justify-start gap-4 mt-4">
+            <CardFooter className="flex flex-col md:flex-row justify-start gap-8 md:gap-20 mt-2">
                 <FormButton text="Generate QR!" />
                 {
                     qrCode &&
                     (
-                        <picture>
-                            <img src={qrCode} alt="QR Code" />
-                        </picture>
+                        <div className='flex gap-4 items-center align-middle'>
+                            <p className='text-2xl italic font-semibold md:block hidden'>Save it and share it!</p>
+                            <ArrowBigRight className='w-16 h-16 md:block hidden' />
+                            <picture className=' w-[250px] h-[250px] aspect-square border'>
+
+                                <img src={qrCode} alt="QR Code" />
+
+                            </picture>
+                        </div>
+
                     )
                 }
+
+
             </CardFooter>
         </form>
     )
