bc85a544eac22635b583b5c68c27e18ee3c93cb8
Othamae <vero.gconesa@gmail.com>
2024-01-31 21:40:48 +0100

    feat: :sparkles: Created Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+	return (
+		<footer className="static bottom-0 flex items-center justify-center py-4 w-full">
+			Build With <span className="text-pink-500 text-xl px-1">&#9825;</span>
+			by&nbsp;
+			<a
+				href="https://othamae.dev"
+				target="_blank"
+				className="underline underline-offset-2"
+			>Othamae
+			</a>
+		</footer>)
+}
+
+export default Footer
+
+
